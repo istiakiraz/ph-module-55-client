d5fe79ac61b6dd884c88a80279253b80bdf02fcd
istiakiraz <istiakiraz@gmail.com>
2025-05-13 03:11:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-2xl font-bold mt-8 text-center' >Simple CURD Application</h1>
+     
+    </>
+  )
+}
+
+export default App
